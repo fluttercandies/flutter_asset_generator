48285b2aec3c86d96a0233964a1551ce62b7e3a5
AlexVincent525 <github@alexv525.com>
2020-04-14 16:59:22 +0800

    :sparkles: Add flutter format after constants file generated.

---
+++ lib/format.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+import 'package:io/ansi.dart';
+
+Future<void> formatFile(File file) async {
+  if (file == null) {
+    return;
+  }
+
+  if (!file.existsSync()) {
+    print(red.wrap('format error: ${file?.absolute?.path} doesn\'t exist\n'));
+    return;
+  }
+
+  processRunSync(
+    executable: 'flutter',
+    arguments: 'format ${file?.absolute?.path}',
+    runInShell: true,
+  );
+}
+
+void processRunSync({
+  String executable,
+  String arguments,
+  bool runInShell = false,
+}) {
+  final ProcessResult result = Process.runSync(
+    executable,
+    arguments.split(' '),
+    runInShell: runInShell,
+  );
+  if (result.exitCode != 0) {
+    throw Exception(result.stderr);
+  }
+  print('${result.stdout}');
+}
